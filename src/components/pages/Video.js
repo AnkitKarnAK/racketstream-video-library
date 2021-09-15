ebc4648bd79f340d3478b9621f8f3144cac309f8
Ankit Karn <ankit.karn.ak@gmail.com>
2021-09-15 22:35:29 +0530

    added like funtionality on video page

--- src/components/pages/Video.js
+++ src/components/pages/Video.js
@@ -1,15 +1,23 @@
-import { FaThumbsUp } from "react-icons/fa";
-import { RiPlayListLine } from "react-icons/ri";
+import { FaThumbsUp, FaRegThumbsUp } from "react-icons/fa";
 import { useNavigate, useParams } from "react-router";
 import { useState, useEffect } from "react";
 import Loader from "react-loader-spinner";
 import { getVideoFromServer } from "../../api/api-requests";
+import { useAuthContext } from "../../context/auth-context";
+import { useDataContext } from "../../context/data-context";
+import { addOrRemoveVideoFromLikedVideos } from "../../api/api-requests";
+import { isAlreadyAdded2 } from "../../context/data-reducer";
 
 const Video = () => {
   const [video, setVideo] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const { videoId } = useParams();
   const navigate = useNavigate();
+  const { isUserLogin, userId } = useAuthContext();
+  const {
+    state: { likedVideos },
+    dispatch,
+  } = useDataContext();
 
   useEffect(() => {
     (async () => {
@@ -52,12 +60,32 @@ const Video = () => {
             <small>{video.viewCount} views</small>
             <div className="savebar">
               <div className="savebar-item">
-                <FaThumbsUp />
-                <div>Like</div>
-              </div>
-              <div className="savebar-item">
-                <RiPlayListLine />
-                <div>Save</div>
+                {isAlreadyAdded2(likedVideos, video._id) ? (
+                  <FaThumbsUp />
+                ) : (
+                  <FaRegThumbsUp />
+                )}
+                <div
+                  onClick={() => {
+                    isUserLogin
+                      ? (async () => {
+                          setIsLoading(true);
+                          const { response } =
+                            await addOrRemoveVideoFromLikedVideos({
+                              userId,
+                              videoItem: video,
+                            });
+                          dispatch({
+                            type: "GET_LIKED_VIDEOS",
+                            payload: response.data.likedVideo,
+                          });
+                          setIsLoading(false);
+                        })()
+                      : navigate("/login");
+                  }}
+                >
+                  {isAlreadyAdded2(likedVideos, video._id) ? "Liked" : "Like"}
+                </div>
               </div>
             </div>
             <div className="channel-description">
